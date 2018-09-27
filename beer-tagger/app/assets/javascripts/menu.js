494d4dd0012a0ae4589ed60096d07ce154a24885
diegotf30 <diegotf30@gmail.com>
2018-09-27 13:40:34 -0500

    Added selection drawing
    When a user selects an area with a tag_name it is drawn on top of the photo

--- beer-tagger/app/assets/javascripts/menu.js
+++ beer-tagger/app/assets/javascripts/menu.js
@@ -1,19 +1,34 @@
 var selections = [];
+var tags = [];
 
 function save_selection() {
-	let tag = $('#menu .tag');
+	let tag = $('#menu .tag').val();
+	if(tag === "") return;
 
-	if(tag.val() !== "") {
-		let selection = new Object();
-		selection.top_left = { x: parseInt(x), y: parseInt(y) }
-		selection.bot_right = { x: parseInt(x2), y: parseInt(y2) }
-		selection.width = w;
-		selection.height = h;
-		selection.tag = tag.val();
-		selections.push(selection);
+	let selection = new Object();
+	selection.top_left = { x: parseInt(x), y: parseInt(y) }
+	selection.bot_right = { x: parseInt(x2), y: parseInt(y2) }
+	selection.width = w;
+	selection.height = h;
+	selection.tag = tag;
+	selections.push(selection);
+
+	// Assign random color to tag class
+	if(!(tag in tags)) {
+		tags[tag] = '#' + Math.floor(Math.random()*16777215).toString(16);
 	}
+	let boxId = selections.length;
+	// Draw selection
+	let box = $.parseHTML(`<div class="box box${boxId}" style="width: ${w}px; height: ${h}px; border:3px solid ${tags[tag]}; z-index: ${99 + boxId};"></div>`);
+    $(box).append($.parseHTML(`<div class="corner" style="background-color:${tags[tag]}; top: -5px; left: -5px; z-index: ${100 + boxId}"></div>`));
+    $(box).append($.parseHTML(`<div class="corner" style="background-color:${tags[tag]}; top: -5px; right: -5px; z-index: ${100 + boxId}"></div>`));
+    $(box).append($.parseHTML(`<div class="corner" style="background-color:${tags[tag]}; bottom: -5px; left: -5px; z-index: ${100 + boxId}"></div>`));
+    $(box).append($.parseHTML(`<div class="corner" style="background-color:${tags[tag]}; bottom: -5px; right: -5px; z-index: ${100 + boxId}"></div>`));
 
-	tag.val(''); //Reset menu input-tag
+	$('.jcrop-tracker').last().append(box);
+
+	// Empty tag name (since menu is hidden, not destroyed)
+	$('#menu .tag').val('');
 	release_jcrop();
 }
 
