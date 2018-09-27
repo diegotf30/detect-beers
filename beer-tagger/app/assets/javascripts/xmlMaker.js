e960a247736991b5ffe9f929fefa75f5a3ba628a
DniGaytan <dnigatan07@gmail.com>
2018-09-27 12:35:19 -0500

    se creo el xmlMaker para convertir selecciones en archivos XML

---
+++ beer-tagger/app/assets/javascripts/xmlMaker.js
@@ -0,0 +1,40 @@
+var xmlList = []; //list that contains all the xml file
+var xmlContent = "";
+
+//file_path
+
+
+function createXml(selectionTagArr, file_path){
+	let xmlFile = "<annotation> \n";
+
+	xmlFile.concat(`<path>` file_path `</path> \n`)
+	xmlFile.concat(`<size> \n` `<width>` ${selectionTagArr[0].width} `</width>` `<height>` ${selectionTagArr[0].height}`</height>` `</size> \n`)
+	let parser = newDOMParser();
+	var xmlDoc;
+
+	for (var i = 0; i < SelectionTagArr.length(); i++) {
+		xmlFile.concat(createXmlObj(SelectionTagArr[i]));
+	}
+
+	xmlFile.concat("\n </annotation>");
+	xmlDoc = parser.parseFromString(xmlFile, "text/xml")
+
+	
+	xmlList.push(xmlDoc);
+}
+
+
+function createXmlObj(selectionTag){
+	let objText = "<object> \n";
+
+	objText.concat(`<name>` ${selectionTag.tag} `</name> \n`);
+	objText.concat(`<pose>Unspecified</pose> \n <truncated>0</truncated> \n <difficult>0</difficult>`);
+	objText.concat(`<bndBox> \n ` `<xmin> ` ${selectionTag.top_left.x} `</xmin> \n`);
+	objText.concat(`<ymin>` ${selectiontTag.top_left.y} `</ymin> \n`);
+	objText.concat(`<xmax>` ${selectionTag.bot_right.x} `</xmax> \n`);
+	objText.concat(`<ymax>` ${selectionTag.bot_right.y} + `</ymax> \n </bndBox \n`);
+	objText.concat(`</object> \n `);
+
+	return objText;
+
+}
