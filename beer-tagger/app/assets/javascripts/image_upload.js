35aa566521e77ffc9b316fed30a372fd53941aba
diegotf30 <diegotf30@gmail.com>
2018-09-28 03:06:58 -0500

    Added js for modal validations
    * New upload to azure SVG
    * Changed way we get file_path

--- beer-tagger/app/assets/javascripts/image_upload.js
+++ beer-tagger/app/assets/javascripts/image_upload.js
@@ -3,22 +3,27 @@ var file_path;
 function preview_image() {
     var preview = $('img')[0]
     var file    = $('#imgUpload')[0].files[0];
-    file_path   = file.mozFullPath;
+    file_path   = $('#imgUpload').val();
     var reader  = new FileReader();
     const isImage = (file) => file['type'].includes('image');
 
     reader.onloadend = function() {
-       preview.src = reader.result;
+      preview.src = reader.result;
     }
 
     if(!isImage(file)) {
       alert('Solo se permiten imagenes');
     }
-    else if (file) {
-       reader.readAsDataURL(file); //previews image
-       setTimeout(setup_jcrop, 35);
+    else if(file) {
+      //Add card info
+      $('.selections ul').empty().append(`<li class="list-group-item empty-selec">No hay ninguna selección</li>`);      
+      $('.empty-paths').remove();
+      $('.paths ul').append(`<li class="list-group-item">${file.name}</li>`);
+
+      reader.readAsDataURL(file); //previews image
+      setTimeout(setup_jcrop, 35);
     }
     else {
-       preview.src = "";
+      preview.src = "";
     }
 }
